b1052e084b38d47a04ab41c34b362cd3ee0ccb95
KevinMeisenbacher <kevinmeisenbacher@gmail.com>
2021-06-23 11:42:28 -0400

    Attempt to nest skills into skill sets

--- routes/about.js
+++ routes/about.js
@@ -4,10 +4,39 @@ var router = express.Router();
 /* GET about page. */
 router.get('/', function(req, res, next) {
     let tools = [
-        {name: 'Express'},
-        {name: 'NodeJS'},
-        {name: 'Handlebars'}];
-  res.render('about', { title: 'About Me', tools: tools });
+        {
+            name: 'HTML',
+            name: 'CSS',
+            name: 'JavaScript'
+        },
+        {
+            name: 'Express',
+            name: 'NodeJS',
+        },
+        {
+            name: 'C#',
+            name: 'ASP.NET',
+            name: 'Java',
+        },
+        {
+            name: 'MYSQL',
+            name: 'SQL Server',
+            name: 'MongoDB'
+    },
+        {
+            name: 'COBOL',
+            name: 'JCL'
+        }
+    ];
+
+    let skillSets = [
+        frontEnd = tools[0],
+        fullStack = tools[1],
+        backEnd = tools[2],
+        database = tools[3],
+        mainframe = tools[4]
+    ];
+  res.render('about', { title: 'About Me', skillSets: skillSets, tools: tools });
 });
 
 module.exports = router;
